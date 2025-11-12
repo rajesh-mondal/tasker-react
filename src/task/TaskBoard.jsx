6a2fb725347ca51fd7d4f0fe859b9d967ebbe75b
Rajesh Mondal <rajesh01749@gmail.com>
2025-11-13 00:45:56 +0600

    implement task list rendering

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -24,7 +24,7 @@ export default function TaskBoard() {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
