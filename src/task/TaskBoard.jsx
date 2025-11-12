b9c470816f941ff6f8df22448bb3d8456872e7a0
Rajesh Mondal <rajesh01749@gmail.com>
2025-11-13 00:22:03 +0600

    set up task state structure

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description:
+      "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
