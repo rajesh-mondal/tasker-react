1e16cb4e3055aa2cf953a2f0b7221a053979a2b7
Rajesh Mondal <rajesh01749@gmail.com>
2026-03-30 22:10:44 +0600

    feat: implement task creation functionality

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -15,15 +16,24 @@ export default function TaskBoard() {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    console.log("Adding a task...", newTask);
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
+  }
+
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         {/* Search Box */}
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
