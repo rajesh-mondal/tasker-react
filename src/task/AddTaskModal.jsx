e7eefc1fdd718a674c50952e055cfb7c36f68298
Rajesh Mondal <rajesh01749@gmail.com>
2026-03-30 22:45:43 +0600

    fix: modal positioning and sizing issues

--- src/task/AddTaskModal.jsx
+++ src/task/AddTaskModal.jsx
@@ -24,13 +24,13 @@ export default function AddTaskModal({ onSave }) {
 
   return (
     <>
-      <div className="bg-black/70 h-full w-full z-10 absolute top-0 left-0"></div>
-      <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/36 bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3">
+      <div className="fixed top-0 left-0 z-50 h-screen w-screen bg-black/70 transition-all"></div>
+      <form className="fixed top-1/2 left-1/2 z-50 w-full max-w-[740px] -translate-x-1/2 -translate-y-1/2 rounded-xl border border-[#FEFBFB]/36 bg-[#191D26] p-9 max-md:px-4 lg:p-11">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
           Add New Task
         </h2>
 
-        <div className="space-y-9 text-white lg:space-y-10">
+        <div className="space-y-7 text-white lg:space-y-8">
           <div className="space-y-2 lg:space-y-3">
             <label htmlFor="title">Title</label>
             <input
@@ -90,7 +90,7 @@ export default function AddTaskModal({ onSave }) {
           </div>
         </div>
 
-        <div className="mt-16 flex justify-center lg:mt-20">
+        <div className="mt-10 flex justify-center lg:mt-12">
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
